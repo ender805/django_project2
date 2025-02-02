7461e942d1803cbf191da445c094c365f9771e1b
Maciej Tuszynski <xtzl9j78@students.codeinstitute.net>
2025-02-02 14:04:26 +0000

    next

--- about/views.py
+++ about/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def about_me(request):
-    return HttpResponse("This would be the about page")
+     if request.method == "GET":
+       return HttpResponse("This was a GET request")
+     elif request.method == "POST":
+        return HttpResponse("This was a POST request")
